db378c1d667a9c9d70dcbd7a71c9e657e78d4dbf
Muhammad Asadullah Qureshi <113957752+asadullah1s@users.noreply.github.com>
2026-09-06 20:57:32 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -342,9 +342,9 @@ Designs shown are for shoot/styling purposes. Actual product color may vary slig
     category: "Jacquard",
     price: "৳8000",
     images: [
-        "images/products/product-39.png",
-        "images/products/product-40.png",
-        "images/products/product-41.png"
+        "images/products/product-39.jpeg",
+        "images/products/product-40.jpeg",
+        "images/products/product-41.jpeg"
     ],
     description: ` Unstitched 3-Piece Suit
 Shirt
